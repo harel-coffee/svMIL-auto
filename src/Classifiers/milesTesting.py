635a38bea603537b7c57b32e2754cd8a94283429
mnieboer <m.m.nieboer@umcutrecht.nl>
2018-03-15 13:31:49 +0100

    The basic MILES classifier can work and get 85% accuracy with real data, but still needs to be improved on, also not all features are present in the file thus the actual performance cannot yet be judged

--- src/Classifiers/milesTesting.py
+++ src/Classifiers/milesTesting.py
@@ -25,15 +25,80 @@ __status__ = "Development"
 
 #5. Classify each instance in a test bag as positive or negative
 
+Current issues:
+	- The output file TN_annotations does not look good (nothing will work right now because there are no degree annotations for the TN data)
+	- Have a better way of handling features that do not exist for some variants 
+
+
+This script is currently just a dump of some code to test the MILES classification, it is in no way in it's final form O-(=_= O-)
+
 """
 
 ### Imports ###
+
+import sys
+sys.path.insert(0, '../')
 
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from inputDataParser import InputDataParser
 
 ### Code ###
+
+def readAnnotationData(annotationFile):
+	"""
+		Just some ugly function that will read an annotation file into a numpy matrix.
+		Will eventually go to its own class if MILES works out for the project
+	
+		I will first limit the method to values that can be used in absolute distances.
+		Later this can be extended to potentially having a different distance per feature type. 
+	
+	"""
+	
+	annotations = []
+	
+	with open(annotationFile, "r") as f:
+		lineCount = 0
+		header = []
+		for line in f:
+			#line = line.strip() #do not do this as it removes entries that are empty if these are at the end of the line
+			#print line
+			splitLine = line.split('\t')
+			if lineCount < 1:
+				header = splitLine
+				lineCount += 1
+				continue
+		
+			#Obtain required data by header information
+			chr1Index = header.index("chr1")
+			s1Index = header.index("s1")
+			e1Index = header.index("e1")
+			chr2Index = header.index("chr2")
+			s2Index = header.index("s2")
+			e2Index = header.index("e2")
+			noOfGenesInWindowIndex = header.index("noOfGenesInWindow")
+			overlappingTadBoundariesIndex = header.index("overlappingTadBoundaries")
+			
+			#For these features we can maybe take the maximum value.
+			hiCDegreeIndex = header.index("hiCDegree")
+			hiCBetweennessIndex = header.index("hiCBetweenness")
+			
+			noOfGenesInWindow = splitLine[noOfGenesInWindowIndex]
+			overlappingTadBoundaries = splitLine[overlappingTadBoundariesIndex]
+			
+			if len(splitLine[hiCDegreeIndex]) > 0:
+				hiCDegree = max(splitLine[hiCDegreeIndex]) #take max for now
+			else:
+				hiCDegree = 0
+			if len(splitLine[hiCBetweennessIndex]) > 0:
+				hiCBetweenness = max(splitLine[hiCBetweennessIndex])
+			else:
+				hiCBetweenness = 0
+			currentAnnotations = [int(noOfGenesInWindow), int(overlappingTadBoundaries), int(hiCDegree), int(hiCBetweenness)]
+			
+			annotations.append(currentAnnotations)	
+
+	return np.matrix(annotations, dtype="int")
 
 # Define data
 patient1P = np.array([[50,150],[60, 200]])
@@ -49,29 +114,71 @@ patient2N = np.array([[10, 5],[14, 3]])
 bags = np.array([patient1P, patient2P, patient1N, patient3P, patient4P, patient2N])
 labels = np.array([1,1,-1, 1, 1, -1])
 
-#Read some test and training data from the real datasets
-
-truePositives = sys.argv[1]
-trueNegatives = sys.argv[2]
-inputDataParser = InputDataParser()
-
-#Some weird naming going on here... 
-tpInputDataParsed = inputDataParser.parseInputData(truePositives)
-preparedTP = inputDataParser.prepareInputData(tpInputDataParsed)
-
-tnInputDataParsed = inputDataParser.parseInputData(trueNegatives)
-preparedTN = inputDataParser.prepareInputData(tnInputDataParsed)
-
-#Subset the data for testing
-#Also only use the features that we can use with the current absolute distance
-
-
-
-# Spilt dataset arbitrarily to train/test sets
 train_bags = bags[3:]
+print "original train_bags: "
+print train_bags
+
 train_labels = labels[3:]
 test_bags = bags[:3]
 test_labels = labels[:3]
+
+###### Real data test
+
+#Read some test and training data from the real datasets (annotated, the output of main.py)
+truePositives = sys.argv[1]
+trueNegatives = sys.argv[2]
+
+#Read the annotation files into a list of features
+### there is still a problem with the files, the output does not look as expected for the TN
+
+tpAnnotations = readAnnotationData(truePositives)
+tnAnnotations = readAnnotationData(trueNegatives)
+
+#Subset the data for training and testing (arbitrary)
+#We need to make some bags
+#There is patient information in a original file, but this is not parsed right now. In the future, each bag can be a patient, and each instance is a variant of the patient. 
+#For now, I will just do that arbitrarily and see what comes out
+
+#Lots of conversions between numpy going on here, it could probably be better
+
+allTpBags = []
+allTnBags = []
+
+#Make 10 bags with 50 instances
+totalBagNum = 10
+instanceNum = 50
+
+currentInstanceNum = 0
+for bagNum in range(0, totalBagNum):
+	
+	tpBag = tpAnnotations[currentInstanceNum:instanceNum, :]
+	allTpBags.append(tpBag)
+	
+	tnBag = tnAnnotations[currentInstanceNum:instanceNum, :]
+	allTnBags.append(tnBag)
+	
+	currentInstanceNum += instanceNum
+	instanceNum += instanceNum
+
+
+allTpBags = np.array(allTpBags)
+
+allTnBags = np.array(allTnBags)
+
+#Determine training/test labels
+
+#Is this still the good format?? It does not really look like the other one anymore, there's now types which is a bit weird
+#Also I should change the naming conventions in the rest of the code to look the same as in the rest of the program
+
+train_bags = np.array([allTpBags[5:,:], allTnBags[5:,:]])
+train_bags = np.concatenate((train_bags[0], train_bags[1]), axis=0) #there must be a better way... but not looking into it now
+train_labels = np.array([1,1,1,1,1,-1,-1,-1,-1,-1]) #some random labels for now
+test_bags = np.array([allTpBags[:5,:], allTnBags[:5,:]])
+test_bags = np.concatenate((test_bags[0], test_bags[1]), axis=0)
+test_labels = train_labels
+
+print "training data:"
+print train_bags
 
 #1. Obtain centers in each bag. Here I will use random points
 centers = []
@@ -85,12 +192,10 @@ for bag in train_bags:
 	randomInstance = bag[randomInstanceInd,:]
 	centers.append(randomInstance)
 	
-	
-print centers
-	
+
 #2. Compute the distance from each cluster center to other points and take the minimum
 #3. Generate a similarity matrix
-similarityMatrix = np.zeros([len(centers), len(bags)])
+similarityMatrix = np.zeros([len(centers), len(train_bags)])
 
 for centerInd in range(0, len(centers)):
 	
@@ -127,7 +232,7 @@ testInstances = np.vstack(test_bags)
 #Do we now compute the distance to the training data to get the same number of features?
 
 
-testSimilarityMatrix = np.zeros([testInstances.shape[0], len(bags)])
+testSimilarityMatrix = np.zeros([testInstances.shape[0], len(test_bags)])
 testLabels = []
 
 #Fix the labels per instance
@@ -163,9 +268,3 @@ score = rfClassifier.score(testSimilarityMatrix, testLabels)
 print score
 
 print rfClassifier.predict(testSimilarityMatrix)
-
-
-
-
-
-
