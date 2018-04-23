e51ec2a17b4055761776f375f66dfcae51134c6d
mnieboer <m.m.nieboer@umcutrecht.nl>
2018-04-23 12:14:48 +0200

    Genes can now be ranked automatically by how many SVs are affecting these genes directly within all samples of 1 tumor type

--- src/CausalGeneRanking/geneRanking.py
+++ src/CausalGeneRanking/geneRanking.py
@@ -19,45 +19,76 @@ class GeneRanking:
 
 	def __init__(self, genes):
 		
-		
-		
-		
 		#1. Get all unique cancer types and map the gene objects to the right cancer type
 		cancerTypes = dict()
-		
+	
+		cancerTypeTotalSVs = dict() #save a number of how many SVs are there in total for each cancer type to make the final scoring matrix. 
 		for gene in genes:
 			
 			if gene.SVs is not None:
 				for sv in gene.SVs:
 					cancerType = sv.cancerType
-					if cancerType not in cancerTypes.keys():
-						cancerTypes[cancerType] = []
+					if cancerType not in cancerTypes:
+						cancerTypes[cancerType] = dict()
+					if cancerType not in cancerTypeTotalSVs:
+						cancerTypeTotalSVs[cancerType] = 0
 					
-					cancerTypes[cancerType].append(sv)
-		
+					#If the gene is not yet in the list of cancer types, add it
+					if gene not in cancerTypes[cancerType]:
+						cancerTypes[cancerType][gene] = []
+					#Also add all SVs to that gene that are relevant for this cancer type.
+					
+					cancerTypes[cancerType][gene].append(sv)
+					cancerTypeTotalSVs[cancerType] += 1
+
+		#For each gene, get the SVs.
+		#Only get the SVs of the right cancer type (can be different per gene)
+					
 		for cancerType in cancerTypes:
+			print "cancer type: ", cancerType
 			cancerTypeSVs = cancerTypes[cancerType] #Use these SVs to map to the right position in the scoring matrix
-			print cancerTypeSVs
+			#print cancerTypeSVs
 			#For each cancer type, loop through the genes.
 			#Define the scoring matrix
 			
-			scoringMatrix = np.empty([len(cancerTypeSVs), len(genes)])
-			
-			for geneInd in range(0, len(genes)):
+			scoringMatrix = np.zeros([cancerTypeTotalSVs[cancerType], len(cancerTypeSVs)])
+			genesInOrder = [] #keep these lists to ensure that after we rank we can get back to the genes that are most interesting. 
+			svsInOrder = []
+			for geneInd in range(0, len(cancerTypeSVs)): #first loop over the genes, then get their SVs
 				
-				gene = genes[geneInd]
-				
+				gene = cancerTypeSVs.keys()[geneInd]
+				genesInOrder.append(gene)
 				#1. Check which genes are directly overlapped by an SV, these get a score of 1 for these SVs.
 				
 				#Perform additional check to see if the gene has SVs at all
 				
-				geneSVs = gene.SVs
+				geneSVs = cancerTypeSVs[gene]
 				
-				for sv in geneSVs:
-					svInd = cancerTypeSVs.index(sv) ##Somehow the SVs here are not in the list, I don't understand why. 
+				for svInd in range(0, len(geneSVs)):
 					
+					#If the SV is not in the list of SVs, it is from a different cancer type, do not score it. 
+
 					scoringMatrix[svInd][geneInd] = 1
-					
-					
-			print scoringMatrix
+					svsInOrder.append(geneSVs[svInd])
+		
+			#print scoringMatrix
+			
+			#Sum the total score per gene and report the genes by which ones are most likely causal.
+			
+			geneScoresSummed = np.sum(scoringMatrix, axis=0)
+			
+			#Sort and report the names of the genes that are involved
+			sortedGenesInd = np.argsort(geneScoresSummed)[::-1]
+		
+			
+			
+			for geneInd in sortedGenesInd:
+				
+				
+				if geneScoresSummed[geneInd] > 20:
+					print genesInOrder[geneInd].name, genesInOrder[geneInd].chromosome, genesInOrder[geneInd].start, ": ", geneScoresSummed[geneInd]	
+		
+			#exit()
+			
+				
 					
