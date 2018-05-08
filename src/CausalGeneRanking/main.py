53e816503441d37cea16bd5ab42ac4ed5abebb07
mnieboer <m.m.nieboer@umcutrecht.nl>
2018-05-08 11:57:36 +0200

    Finalized ranking of genes

--- src/CausalGeneRanking/main.py
+++ src/CausalGeneRanking/main.py
@@ -82,7 +82,7 @@ def readCausalGeneFile(causalGeneFile):
 
 causalGeneFile = sys.argv[1]
 causalGenes = readCausalGeneFile(causalGeneFile)
-
+uniqueCancerTypes = []
 #1.2 Also read the SVs here already, so that we can provide a random set of SVs to the same function later on
 def getSVsFromFile(svFile):
 		
@@ -117,6 +117,10 @@ def getSVsFromFile(svFile):
 			
 			cancerType = splitLine[cancerTypeIndex]
 			sampleName = splitLine[sampleNameIndex]
+			
+			
+			if cancerType not in uniqueCancerTypes:
+				uniqueCancerTypes.append(cancerType)
 	
 			#If the coordinates are missing on the second chromosome, we use the coordinates of the first chromosome unless chr 1 and chr 2 are different.
 			if splitLine[chr1Index] == splitLine[chr2Index]:
@@ -144,7 +148,7 @@ def getSVsFromFile(svFile):
 	regions = np.array(variantsList, dtype='object')
 	
 	return regions		
-			
+
 svFile = "../../data/TPTNTestSet/TP.txt" #should be a setting
 svData = getSVsFromFile(svFile)
 
@@ -188,130 +192,147 @@ print hg19Coordinates
 #For the current chromosome we get the maximum decrease/increase that this SV can get
 #The new coordinate can be derived randomly from this range
 
-shuffledSvs = []
+noOfPermutations = 5
 
-for sv in svData:
-	
-	
-	#1. Get the min max bounds for the chromosomes (2 in case of translocation)
-	#The minimum start coordinate is just 1, the maximum start is the length-start
-	#The end coordinate is just start + length
-	
-	chromosome1 = sv[0]
-	chromosome2 = sv[3]
-	
-	chr1Length = hg19Coordinates[chromosome1]
-	chr2Length = hg19Coordinates[chromosome2]
-	
-	start1 = int(sv[1])
-	start2 = int(sv[2])
-	
-	startDifference = start2 - start1
-	
-	end1 = int(sv[4])
-	end2 = int(sv[5])
-	
-	endDifference = end2 - end1
-	
-	minimumStart1 = 1	
-	maximumStart1 = chr1Length - start1
-	
-	#If the start position is after the bounds of the chromosome, the maximum start should be the length of the chromosome - the length of the SV.
-
-	if maximumStart1 < 0:
-		maximumStart1 = chr1Length - svLength
-
-	newStart1 = random.randint(minimumStart1, maximumStart1)
-	newStart2 = newStart1 + startDifference
-	
-	#If the chromosomes are the same, use the maximum start for chr1. Otherwise use different chromosomes.
-	if chromosome1 == chromosome2:
-	
-		#difference between end1 and start 1
-		svLength = end1 - start1
-			
-		newEnd1 = newStart1 + svLength
-		newEnd2 = newEnd1 + endDifference
-		
-	else: #If the chromosomes are not the same, the start and end coordinates do not necessarily need to be equidistant. Here we can randomly sample the start on both chromosomes 		
-		maximumStart2 = chr2Length - start2
-		
-		#If the chr2 start is outside of the bounds, we only need to subtract the difference between the positions on chromosome 2. 
-		if maximumStart2 < 0:
-			maximumStart2 = chr2Length - endDifference
-		
-		#The end coordinate here is actually the start coordinate for chromosome 2.
-		newEnd1 = random.randint(minimumStart1, maximumStart2) #chr2 has the same start coordinate as chr 1
-		newEnd2 = newEnd1 + endDifference
-		
-	#Sample name and cancer type can be copied from the other SV. Chromosome information also remains the same.
-	#Keep in mind that the sample name and cancer type are reversed in the SV object beause that order makes more sense. 
-	newSvObj = SV(chromosome1, newStart1, newStart2, chromosome2, newEnd1, newEnd2, sv[7], sv[6])
-	newSv = [chromosome1, newStart1, newStart2, chromosome2, newEnd1, newEnd2, sv[6], sv[7], newSvObj]	
-	shuffledSvs.append(newSv)	
-
-shuffledSvs = np.array(shuffledSvs, dtype="object")	
-
-#5. Re-obtain the gene neighborhood and the SVs overlapping these (could be optimized, we only need to re-associate the SVs)
-print shuffledSvs
-
-#We have to make sure here that the objects will not get overlaps with previous objects
-print "Defining the neighborhood for the causal genes and shuffled SVs"
-NeighborhoodDefiner(causalGenes, shuffledSvs)
-
-#6. Rank the shuffled genes
-print "Ranking the genes for the shuffled SVs"
-geneRanking = GeneRanking(causalGenes[:,3])
-
-#7. Compute p-values
-
-#For each cancer type, get the scores of the genes after shuffling.
-
-#We wish to have a distribution per gene, so it would be ideal to score the scores of all permutations per gene instead, rather than per permutation.
-
-#A dictionary will suffice, per cancer type an array of permutation scores.
-
-scores = geneRanking.scores
-
+#Prepare placeholder for scores
 perGeneScores = dict()
-geneIndices = dict()
-geneIndex = 0
-permutationInd = 0
-noOfPermutations = 10
-for cancerType in scores:
-	
-	cancerTypeScores = scores[cancerType]
-	
-	print cancerTypeScores
+for cancerType in uniqueCancerTypes:
 	perGeneScores[cancerType] = dict()
 	perGeneScores[cancerType]["geneScore"] = np.zeros([len(causalGenes), noOfPermutations])
 	perGeneScores[cancerType]["eQTLScore"] = np.zeros([len(causalGenes), noOfPermutations])
 	perGeneScores[cancerType]["tadScore"] = np.zeros([len(causalGenes), noOfPermutations])
-	
-	for row in range(0, cancerTypeScores.shape[0]):
-		gene = cancerTypeScores[row][0]
-		geneName = gene.name
-		
-		#make a matrix of genes by permutations
-		
-		#Keep the index of this gene in the matrix
-		geneIndices[geneName] = geneIndex
-		
 
-		perGeneScores[cancerType]["geneScore"][geneIndex, permutationInd] = cancerTypeScores[row, 1]
-		perGeneScores[cancerType]["eQTLScore"][geneIndex, permutationInd] = cancerTypeScores[row, 2]
-		perGeneScores[cancerType]["tadScore"][geneIndex, permutationInd] = cancerTypeScores[row, 3]
+#Also prepare the order of the genes in the scoring matrix
+geneIndices = dict()
+geneIndex = 0
+for gene in causalGenes[:,3]:
 	
-		geneIndex += 1 #for the next iteration of permutations, make sure that we use the right gene indices that we previously stored
+	geneName = gene.name
+	geneIndices[geneName] = geneIndex
+	geneIndex += 1
+
+for permutationRound in range(0, noOfPermutations):
 	
-	print perGeneScores
+	shuffledSvs = []
+	
+	for sv in svData:
+
+		#1. Get the min max bounds for the chromosomes (2 in case of translocation)
+		#The minimum start coordinate is just 1, the maximum start is the length-start
+		#The end coordinate is just start + length
+		
+		chromosome1 = sv[0]
+		chromosome2 = sv[3]
+		
+		chr1Length = hg19Coordinates[chromosome1]
+		chr2Length = hg19Coordinates[chromosome2]
+		
+		start1 = int(sv[1])
+		start2 = int(sv[2])
+		
+		startDifference = start2 - start1
+		
+		end1 = int(sv[4])
+		end2 = int(sv[5])
+		
+		endDifference = end2 - end1
+		
+		minimumStart1 = 1	
+		maximumStart1 = chr1Length - start1
+		
+		#If the start position is after the bounds of the chromosome, the maximum start should be the length of the chromosome - the length of the SV.
+	
+		if maximumStart1 < 0:
+			maximumStart1 = chr1Length - svLength
+	
+		newStart1 = random.randint(minimumStart1, maximumStart1)
+		newStart2 = newStart1 + startDifference
+		
+		#If the chromosomes are the same, use the maximum start for chr1. Otherwise use different chromosomes.
+		if chromosome1 == chromosome2:
+		
+			#difference between end1 and start 1
+			svLength = end1 - start1
+				
+			newEnd1 = newStart1 + svLength
+			newEnd2 = newEnd1 + endDifference
+			
+		else: #If the chromosomes are not the same, the start and end coordinates do not necessarily need to be equidistant. Here we can randomly sample the start on both chromosomes 		
+			maximumStart2 = chr2Length - start2
+			
+			#If the chr2 start is outside of the bounds, we only need to subtract the difference between the positions on chromosome 2. 
+			if maximumStart2 < 0:
+				maximumStart2 = chr2Length - endDifference
+			
+			#The end coordinate here is actually the start coordinate for chromosome 2.
+			newEnd1 = random.randint(minimumStart1, maximumStart2) #chr2 has the same start coordinate as chr 1
+			newEnd2 = newEnd1 + endDifference
+			
+		#Sample name and cancer type can be copied from the other SV. Chromosome information also remains the same.
+		#Keep in mind that the sample name and cancer type are reversed in the SV object beause that order makes more sense. 
+		newSvObj = SV(chromosome1, newStart1, newStart2, chromosome2, newEnd1, newEnd2, sv[7], sv[6])
+		newSv = [chromosome1, newStart1, newStart2, chromosome2, newEnd1, newEnd2, sv[6], sv[7], newSvObj]	
+		shuffledSvs.append(newSv)	
+	
+	shuffledSvs = np.array(shuffledSvs, dtype="object")	
+	
+	#5. Re-obtain the gene neighborhood and the SVs overlapping these (could be optimized, we only need to re-associate the SVs)
+	print shuffledSvs
+	
+	#We have to make sure here that the objects will not get overlaps with previous objects
+	print "Defining the neighborhood for the causal genes and shuffled SVs"
+	NeighborhoodDefiner(causalGenes, shuffledSvs)
+	
+	#6. Rank the shuffled genes
+	print "Ranking the genes for the shuffled SVs"
+	geneRanking = GeneRanking(causalGenes[:,3])
+
+	
+	#For each cancer type, get the scores of the genes after shuffling.
+	
+	#We wish to have a distribution per gene, so it would be ideal to score the scores of all permutations per gene instead, rather than per permutation.
+	
+	#A dictionary will suffice, per cancer type an array of permutation scores.
+	
+	scores = geneRanking.scores
+	
+	permutationInd = permutationRound
+	for cancerType in scores:
+		
+		cancerTypeScores = scores[cancerType]
+		
+		
+		for row in range(0, cancerTypeScores.shape[0]):
+			gene = cancerTypeScores[row][0]
+			geneName = gene.name
+			
+			#make a matrix of genes by permutations
+			
+			
+			
+			#Get the right index for this gene
+			geneIndex = geneIndices[geneName]
+			
+	
+			perGeneScores[cancerType]["geneScore"][geneIndex, permutationInd] = cancerTypeScores[row, 1]
+			perGeneScores[cancerType]["eQTLScore"][geneIndex, permutationInd] = cancerTypeScores[row, 2]
+			perGeneScores[cancerType]["tadScore"][geneIndex, permutationInd] = cancerTypeScores[row, 3]
+		
+		
+		print perGeneScores
+
+
+#7. Compute the p-values
 
 #Check how many of the permutation scores for this gene are larger than the observed gene score for this gene.
 #We can compute this separately per layer, and then rank them based on having the highest score in most columns. 
-	
+print "Computing p-values and ranking genes: " 	
 for cancerType in realScores:
-	
+
 	cancerTypeScores = realScores[cancerType]
+	cancerTypePValues = np.empty([cancerTypeScores.shape[0], 7], dtype="object") #for all genes, store the gene identifier, and 3 columns for the layers.  
+	
+	#For each cancer type keep an array with the scores in the columns. Then do a sorting where the scores are the highest across all rows for that gene. 
 	
 	for row in range(0, cancerTypeScores.shape[0]):
 	
@@ -330,20 +351,42 @@ for cancerType in realScores:
 		permutedTADScores = perGeneScores[cancerType]["tadScore"][geneIndex]
 		
 		#First compute the p-value for the gene score layer
-		proportion = (np.sum((permutedGeneScores >= geneScore).astype(int)) + 1) / (len(permutedGeneScores) + 1) #I think we need equals, when the sum is the same, the value should be TRUE and receive a lower p-value. 
+		proportion = (np.sum((permutedGeneScores >= geneScore).astype(int)) + 1) / float(len(permutedGeneScores) + 1) #I think we need equals, when the sum is the same, the value should be TRUE and receive a lower p-value. 
 		
-		print "gene: ", geneName
-		print "p-value for the gene layer: ", proportion
+		if proportion < 1:
+		
+			print "gene: ", geneName
+			print "p-value for the gene layer: ", proportion
 		
 			
+		eQTLProportion = (np.sum((permutedEQTLScores >= eQTLScore).astype(int)) + 1) / float(len(permutedEQTLScores) + 1) 
+		
+		if eQTLProportion < 1:
+		
+			print "p-value for the eQTL layer: ", eQTLProportion
 		
 		
+		tadProportion = (np.sum((permutedTADScores >= tadScore).astype(int)) + 1) / float(len(permutedTADScores) + 1) 
+		
+		if tadProportion < 1:
+		
+			print "p-value for the TAD layer: ", tadProportion
 		
 		
+		cancerTypePValues[row][0] = gene.name
+		cancerTypePValues[row][1] = gene.chromosome
+		cancerTypePValues[row][2] = gene.start
+		cancerTypePValues[row][3] = proportion
+		cancerTypePValues[row][4] = eQTLProportion
+		cancerTypePValues[row][5] = tadProportion
+
+		#Compute a total score to sort by. 
+		totalScore = proportion + eQTLProportion + tadProportion
+		cancerTypePValues[row][6] = totalScore
+
+
+	#Rank by the total score and report the genes.
+	np.set_printoptions(threshold=np.nan)
+	sortedPValues = cancerTypePValues[cancerTypePValues[:,6].argsort()]
 	
-
-
-
-		
-	
-
+	print sortedPValues
