030af7a501ab00e8df3d03f68b23860d57e5f96d
mnieboer <m.m.nieboer@umcutrecht.nl>
2018-05-09 15:03:39 +0200

    Added sh scripts to parallelize running permutations

---
+++ src/CausalGeneRanking/runRankingWithPermutations.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#$ -V
+#$ -S /bin/bash
+#$ -cwd
+#$ -m as
+#$ -M m.m.nieboer@umcutrecht.nl
+#$ -l h_vmem=4G
+#$ -l h_rt=00:10:00
+#$ -e ranking_permutations_err
+#$ -o ranking_permutations_out
+
+#Run 1 normal run without permutations
+
+#Then run the permutation runs 1000 times, create jobs for that. 
+
+causalGeneFile="$1"
+
+uuid=$(uuidgen)
+
+qsub runRanking.py "$causalGeneFile" "$uuid"
+
+permutationsYN="$2"
+
+
+#qsub runRankingPermutations.sh "$causalGeneFile" "$uuid" "$permutationsYN" "$i" #use job array and inside this script SGE TASK ID to get the run number
