b3b0be1289b074790b47e850848a61b690364b04
mnieboer <m.m.nieboer@umcutrecht.nl>
2019-05-15 16:24:21 +0200

    Functions for analyzing concept genes

--- src/CausalGeneRanking/runMiles.sh
+++ src/CausalGeneRanking/runMiles.sh
@@ -6,7 +6,7 @@
 #$ -M m.m.nieboer@umcutrecht.nl
 #$ -l h_vmem=25G
 #$ -l h_rt=48:00:00
-#$ -e miles_svm_err
-#$ -o miles_svm_out
+#$ -e miles_lasso2_err
+#$ -o miles_lasso2_out
 
 python milesAllBags.py
