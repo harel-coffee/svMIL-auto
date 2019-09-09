7ea280c01b5b7546c32d6b8518e73fa695f7b31f
mnieboer <m.m.nieboer@umcutrecht.nl>
2019-09-09 15:36:41 +0200

    plotting expression of negative and positive set

--- src/CausalGeneRanking/inputParser.py
+++ src/CausalGeneRanking/inputParser.py
@@ -110,17 +110,20 @@ class InputParser:
 				# 	rangeTypeMatch = re.search("range", svType, re.IGNORECASE)
 				# 	if interChrTypeMatch is None and transTypeMatch is None and rangeTypeMatch is None:
 				# 		continue
-				# 
-				#UCEC case
-				if svType != "del" and svType != "invers" and svType != "tandem_dup" and svType != "DEL" and svType != "INV" and svType != "DUP":
-					
-					interChrTypeMatch = re.search("chr", svType, re.IGNORECASE)
-					transTypeMatch = re.search("trans", svType, re.IGNORECASE)
-					rangeTypeMatch = re.search("range", svType, re.IGNORECASE)
-					itxTypeMatch = re.search("ITX", svType, re.IGNORECASE)
-					ctxTypeMatch = re.search("CTX", svType, re.IGNORECASE)
-					if interChrTypeMatch is None and transTypeMatch is None and rangeTypeMatch is None and itxTypeMatch is None and ctxTypeMatch is None:
-						continue
+				#
+				
+				if svType != "del":
+					continue
+				
+				# if svType != "del" and svType != "invers" and svType != "tandem_dup" and svType != "DEL" and svType != "INV" and svType != "DUP":
+				# 	
+				# 	interChrTypeMatch = re.search("chr", svType, re.IGNORECASE)
+				# 	transTypeMatch = re.search("trans", svType, re.IGNORECASE)
+				# 	rangeTypeMatch = re.search("range", svType, re.IGNORECASE)
+				# 	itxTypeMatch = re.search("ITX", svType, re.IGNORECASE)
+				# 	ctxTypeMatch = re.search("CTX", svType, re.IGNORECASE)
+				# 	if interChrTypeMatch is None and transTypeMatch is None and rangeTypeMatch is None and itxTypeMatch is None and ctxTypeMatch is None:
+				# 		continue
 				
 				# if svType != "invers":
 				# 	continue
