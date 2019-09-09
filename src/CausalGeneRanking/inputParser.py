d469aecd8b970130a4a021d3f0f39dc319150394
mnieboer <m.m.nieboer@umcutrecht.nl>
2019-09-09 16:04:12 +0200

    plotting expression of negative and positive set

--- src/CausalGeneRanking/inputParser.py
+++ src/CausalGeneRanking/inputParser.py
@@ -95,13 +95,13 @@ class InputParser:
 				# 	continue
 				
 				#  
-				# interChrTypeMatch = re.search("chr", svType, re.IGNORECASE)
-				# transTypeMatch = re.search("trans", svType, re.IGNORECASE)
-				# rangeTypeMatch = re.search("range", svType, re.IGNORECASE)
-				# 
-				# if interChrTypeMatch is None and transTypeMatch is None and rangeTypeMatch is None:
-				# 	continue
-				# 
+				interChrTypeMatch = re.search("chr", svType, re.IGNORECASE)
+				transTypeMatch = re.search("trans", svType, re.IGNORECASE)
+				rangeTypeMatch = re.search("range", svType, re.IGNORECASE)
+				
+				if interChrTypeMatch is None and transTypeMatch is None and rangeTypeMatch is None:
+					continue
+				
 				# only keep the main 4 types
 				# if svType != "del" and svType != "invers" and svType != "tandem_dup":
 				# 	
@@ -112,8 +112,7 @@ class InputParser:
 				# 		continue
 				#
 				
-				if svType != "invers":
-					continue
+		
 				
 				# if svType != "del" and svType != "invers" and svType != "tandem_dup" and svType != "DEL" and svType != "INV" and svType != "DUP":
 				# 	
