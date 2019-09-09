57f2a9d8f6b4025f0066284442361d1119d77f5f
mnieboer <m.m.nieboer@umcutrecht.nl>
2019-09-09 17:38:06 +0200

    plotting expression of negative and positive set

--- src/CausalGeneRanking/plotPerRulePerformance.py
+++ src/CausalGeneRanking/plotPerRulePerformance.py
@@ -315,9 +315,9 @@ plt.savefig('gains_deg_counts_addition.svg')
 
 #DEGs
 #eqtls, enhancers, promoters, CpG, TFs, HiC, histones, dnaseI
-y = [X, 97, 110, ]
+y = [X, 97, 110, 118, ]
 x = range(0, len(y))
-averages = [X, 3280, 3949, 4251] #total number of pairs
+averages = [X, 3280, 3949, 4251, 4692] #total number of pairs
 
 fig, ax = plt.subplots()
 ax.stem(x, y)
