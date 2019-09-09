3b2b7264c338b730d47adb7174958b8847cc1253
mnieboer <m.m.nieboer@umcutrecht.nl>
2019-09-09 16:01:29 +0200

    plotting expression of negative and positive set

--- src/CausalGeneRanking/inputParser.py
+++ src/CausalGeneRanking/inputParser.py
@@ -112,7 +112,7 @@ class InputParser:
 				# 		continue
 				#
 				
-				if svType != "tandem_dup":
+				if svType != "invers":
 					continue
 				
 				# if svType != "del" and svType != "invers" and svType != "tandem_dup" and svType != "DEL" and svType != "INV" and svType != "DUP":
