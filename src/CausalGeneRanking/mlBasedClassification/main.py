b6264a31ffc774ad988de251fcafb3ae61ec4e2e
mnieboer <m.m.nieboer@umcutrecht.nl>
2020-03-11 14:21:24 +0100

    some xtra documentation for the main interface

--- src/CausalGeneRanking/mlBasedClassification/main.py
+++ src/CausalGeneRanking/mlBasedClassification/main.py
@@ -235,7 +235,7 @@ def cvClassification(similarityMatrix, bagLabels, clf, svType, title):
 	aucs = []
 	mean_fpr = np.linspace(0, 1, 100)
 	
-	fig, ax = plt.subplots()
+	#fig, ax = plt.subplots()
 	for i, (train, test) in enumerate(kfold.split(similarityMatrix, bagLabels)):
 		clf.fit(similarityMatrix[train], bagLabels[train])
 		viz = plot_roc_curve(clf, similarityMatrix[test], bagLabels[test],
@@ -250,33 +250,31 @@ def cvClassification(similarityMatrix, bagLabels, clf, svType, title):
 	print('mean auc: ', np.mean(aucs))
 	print('std of auc: ', np.std(aucs))
 
-	ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
-			label='Chance', alpha=.8)
-	
-	mean_tpr = np.mean(tprs, axis=0)
-	mean_tpr[-1] = 1.0
-	mean_auc = auc(mean_fpr, mean_tpr)
-	std_auc = np.std(aucs)
-	
-	ax.plot(mean_fpr, mean_tpr, color='b',
-			label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (np.mean(aucs), np.std(aucs)),
-			lw=2, alpha=.8)
-	
-	std_tpr = np.std(tprs, axis=0)
-	tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
-	tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-	ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
-					label=r'$\pm$ 1 std. dev.')
-	
-	ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
-		   title="Receiver operating characteristic: " + title)
-	ax.legend(loc="lower right")
-	plt.tight_layout()
-	plt.savefig('miles_' + svType + '.svg')
-	plt.show()
+	# ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
+	# 		label='Chance', alpha=.8)
+	# 
+	# mean_tpr = np.mean(tprs, axis=0)
+	# mean_tpr[-1] = 1.0
+	# mean_auc = auc(mean_fpr, mean_tpr)
+	# std_auc = np.std(aucs)
+	# 
+	# ax.plot(mean_fpr, mean_tpr, color='b',
+	# 		label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (np.mean(aucs), np.std(aucs)),
+	# 		lw=2, alpha=.8)
+	# 
+	# std_tpr = np.std(tprs, axis=0)
+	# tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+	# tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+	# ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
+	# 				label=r'$\pm$ 1 std. dev.')
+	# 
+	# ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
+	# 	   title="Receiver operating characteristic: " + title)
+	# ax.legend(loc="lower right")
+	# plt.tight_layout()
+	# plt.savefig('miles_' + svType + '_oldDupOpt.svg')
+	#plt.show()
 
-	return [0,0]
-	
 
 #do RF optimization with random parameter search
 if optimize == True:
@@ -339,7 +337,9 @@ if test == True:
 		classifier = RandomForestClassifier(n_estimators= 600, min_samples_split=5, min_samples_leaf=1, max_features='auto', max_depth=80, bootstrap=True)
 		title = 'deletions'
 	elif svType == 'DUP':
-		classifier = RandomForestClassifier(n_estimators= 600, min_samples_split=2, min_samples_leaf=2, max_features='sqrt', max_depth=110, bootstrap=False)
+		#classifier = RandomForestClassifier(n_estimators= 600, min_samples_split=2, min_samples_leaf=2, max_features='sqrt', max_depth=110, bootstrap=False)
+		classifier = RandomForestClassifier(n_estimators= 600, min_samples_split=5, min_samples_leaf=1, max_features='auto', max_depth=80, bootstrap=True)
+		#classifier = RandomForestClassifier(n_estimators= 1200, min_samples_split=2, min_samples_leaf=4, max_features='sqrt', max_depth=70, bootstrap=False)
 		title = 'duplications'
 	elif svType == 'INV':
 		classifier = RandomForestClassifier(n_estimators= 200, min_samples_split=5, min_samples_leaf=4, max_features='auto', max_depth=10, bootstrap=True)
