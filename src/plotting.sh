a5e2ad64473d9342b064a9d032aacb72188ed0d0
mnieboer <m.m.nieboer@umcutrecht.nl>
2021-01-18 13:47:05 +0100

    added figure 3 generation code to plotting script

--- src/plotting.sh
+++ src/plotting.sh
@@ -1,30 +1,45 @@
-#!/bin/bash
-#SBATCH --mem=40G
-#SBATCH --time=24:00:00
-#SBATCH -o workflow.out
-#SBATCH -e workflow.err
-#SBATCH --mail-type=FAIL
-#SBATCH --mail-user=m.m.nieboer@umcutrecht.nl
+# Script to create all paper figures for svMIL2.
+
+##### REQUIRED RUNS #####
+#1. Run svMIL2 to create all output needed for figures.
+
+run=false
+if $run; then
+	#These are all our cancer types
+	types=('Breast' 'Colorectal' 'Esophagus' 'Lung' 'Kidney' 'NervousSystem' 'Ovary' 'Pancreas' 'Prostate' 'Skin' 'Uterus' 'UrinaryTract')
+
+	#This will run all swaps, including the 'normal' non-swap run, e.g. Breast + hmec
+	#To run only the non-swaps, see runSwaps.sh
+	for type in ${types[@]}; do
+		sh runSwaps.sh "$type"
+	done
+fi
+
+#### PLOTTING #####
+
+## TO DO ###
+
+#
+#run=true
+#
+#if $run; then
+#	runFolder='./plotting/'
+#	settingsFolder='./settings/settings_HMF_Breast' #we need access to ?
+#
+#	#figure 1 plots
+#	python "$runFolder/plotFrequentCosmicGenes.py" "$settingsFolder"
+#
+#fi
+
+### FIGURE 3 ###
 
 run=false
 
 if $run; then
-	runFolder='./multipleInstanceLearning/'
-	settingsFolder='./settings/settings_HMF_Breast'
+	runFolder='./plotting/'
+	outputFolder='./output/'
 
-	#figure 1 plots
-	python "$runFolder/plotFrequentCosmicGenes.py" "$settingsFolder"
+	python "$runFolder/figure3.py" "$outputFolder"
 
-fi
-
-run=true
-
-if $run; then
-	runFolder='./multipleInstanceLearning/'
-	settingsFolder='./settings/settings_HMF_Breast'
-
-	#figure 2
-
-	#first run the classifier to generate full similarity matrices
 
 fi
